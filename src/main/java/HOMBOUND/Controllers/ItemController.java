0b362042f97debbd0feb89512963aea6b7510314
janeth <jarri023@fiu.edu>
2020-07-18 20:46:47 -0400

    user can now update items only if they were the one who made the requesrt

--- src/main/java/HOMBOUND/Controllers/ItemController.java
+++ src/main/java/HOMBOUND/Controllers/ItemController.java
@@ -1,15 +1,16 @@
 package HOMBOUND.Controllers;
 
 import HOMBOUND.Models.HomBoundRequest;
+import HOMBOUND.Models.HomBoundUser;
 import HOMBOUND.Models.Item;
 import HOMBOUND.Models.enums.HomBoundStatus;
 import HOMBOUND.Models.enums.ItemType;
 import HOMBOUND.Repositories.HomBoundRequestRepository;
+import HOMBOUND.Repositories.HomBoundUserRepository;
 import HOMBOUND.Repositories.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.swing.text.html.Option;
 import java.util.Arrays;
 import java.util.Optional;
 
@@ -23,6 +24,9 @@ public class ItemController {
     @Autowired
     public HomBoundRequestRepository homBoundRequestRepository;
 
+    @Autowired
+    public HomBoundUserRepository homBoundUserRepository;
+
     @PostMapping(path="/{requestId}/add")
     public Item addItemToRequest(@PathVariable("requestId") Long requestId, @RequestBody Item item){
         Optional<HomBoundRequest> request = homBoundRequestRepository.findById(requestId);
@@ -32,26 +36,31 @@ public class ItemController {
     }
 
     @PutMapping(path="/{requestId}/item/{itemId}/update")
-    public Item updateItemInRequest(@PathVariable("requestId") Long requestId, @PathVariable("itemId") Long itemId , @RequestBody Item item){
-        Optional<HomBoundRequest> request = homBoundRequestRepository.findById(requestId);
-        checkForRequestErrors(requestId, item);
+    public Item updateItemInRequest(@PathVariable("requestId") Long requestId, @PathVariable("itemId") Long itemId , @RequestBody ItemWithUserId itemWithUserId){
         Optional<Item> itemToUpdate = itemRepository.findById(itemId);
         if(!itemToUpdate.isPresent()){
             throw new Error("Item with id " + itemId + " does not exist!");
         }
-        if(item.getName() != null){
-            itemToUpdate.get().setName(item.getName());
+        if(canUserUpdate(itemWithUserId.userId, requestId)) {
+            Item item = itemWithUserId.item;
+            if (item.getCategory() != null) {
+                itemToUpdate.get().setCategory(item.getCategory());
+            }
+            checkForRequestErrors(requestId, itemToUpdate.get());
+            if (item.getName() != null) {
+                itemToUpdate.get().setName(item.getName());
+            }
+
+            if (item.getQuantity() != null) {
+                itemToUpdate.get().setQuantity(item.getQuantity());
+            }
+            return itemRepository.save(itemToUpdate.get());
+        } else {
+            throw new Error("User " + itemWithUserId.userId + " does not have permission to edit this request.");
         }
-        if(item.getCategory() != null){
-            itemToUpdate.get().setCategory(item.getCategory());
-        }
-        if(item.getQuantity() != null){
-            itemToUpdate.get().setQuantity(item.getQuantity());
-        }
-        return itemRepository.save(itemToUpdate.get());
     }
 
-    public void checkForRequestErrors(Long requestId,Item item){
+    public void checkForRequestErrors(Long requestId, Item item){
         Optional<HomBoundRequest> request = homBoundRequestRepository.findById(requestId);
         if (!request.isPresent()) {
             String errMsg = "Could not find HomBound Request with id " + requestId + "!";
@@ -68,4 +77,30 @@ public class ItemController {
             throw new Error(errMsg);
         }
     }
+
+    public boolean canUserUpdate(Long userId, Long requestId){
+        Optional<HomBoundRequest> request = homBoundRequestRepository.findById(requestId);
+        Optional<HomBoundUser> user = homBoundUserRepository.findById(userId);
+        if(!request.isPresent() || !user.isPresent()) throw new Error("Could not find user " + userId + " or request " + requestId + " in order to verify permission.");
+        return user.get().getId() == request.get().getRequestedBy().getId();
+    }
+
+    // Custom Types
+    private static class ItemWithUserId {
+        public Long userId;
+        public Item item;
+        public ItemWithUserId(Long userId, Item item) {
+            this.userId = userId;
+            this.item = item;
+        }
+    }
+    private static class RequestWithUserId {
+        public Long userId;
+        public HomBoundRequest request;
+        public RequestWithUserId(Long userId, HomBoundRequest request) {
+            this.userId = userId;
+            this.request = request;
+        }
+    }
 }
+
