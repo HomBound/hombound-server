e529a3f89b1d93fd65ba524c84ac416e2e3905cb
Roland <roland.aroche@gmail.com>
2020-07-19 08:27:46 -0400

    test to redeploy in Heroku

--- src/main/java/HOMBOUND/Services/UserService.java
+++ src/main/java/HOMBOUND/Services/UserService.java
@@ -44,7 +44,7 @@ public class UserService {
         System.out.println("Login Method");
 
         HomBoundUser dbUser = userRepository.findByUsername(user.getUsername());
-        it
+
 
 
         if (dbUser != null &&  passwordEncoder.matches(user.getPassword(), dbUser.getPassword()) ){
